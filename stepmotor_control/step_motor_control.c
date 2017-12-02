85fb8353ea9ecd51f3fa3ba8017f9ec0348cd790
mozjiang <jms292569413@sina.com>
2017-12-03 02:38:15 +0800

    modified

--- stepmotor_control/step_motor_control.c
+++ stepmotor_control/step_motor_control.c
@@ -2,8 +2,6 @@
 #include "intrins.h"
 #define uchar unsigned char
 #define uint unsigned int
-//#define th0 0xfc
-//#define tl0 0x18
 //void delay1ms(void);
 //void delay_nms(unsigned short int t);
 void init(void);
@@ -11,35 +9,50 @@ uchar code turn[8] = {0x08,0x0c,0x04,0x06,0x02,0x03,0x01,0x09}; //45 degree per 
 
 
 uchar i = 0;
-bit turn_direction_flag = 0;
-bit start_stop_flag = 1;
+bit turn_direction_flag = 1;   // 0 - backward ; 1 - forward;
+//bit start_stop_flag = 0; // 0 - stop;1 - start;
+int length = 0;
 int main()
 {
 	init();
 	while(1){
-
+		if(length > 8000){
+			TR0 = ~TR0;
+			TR1 = ~TR1;
+			
+			turn_direction_flag = 0;
+			length = 8000;
+			
+		}
+		
+		if(length < 0){
+			TR0 = ~TR0;
+			TR1 = ~TR1;
+			
+			turn_direction_flag = 1;
+			length = 0;
+			
+		}
 	}
-	return 0;
 }
 
 void init(){
 
-	TMOD = 0x01;
-	TH0 = 0xfc;
+	TMOD = 0x11;
+	TH0 = 0xfc;  //1ms
 	TL0 = 0x18;
-	//TH1 = 0x3c;
-	//TL1 = 0xb0;
 	
 	EA = 1;
 	ET0 = 1;
-	//ET1 = 1;
-	//EX0 = 1;
+	ET1 = 1;
+	EX0 = 1;
+	EX1 = 1;
 	
-	//IT0 = 1;
-	//IT1 = 1;
+	IT0 = 1;
+	IT1 = 1;
 	
-	TR0 = 1;
-	//TR1 = 1;
+	TR0 = 0;
+	TR1 = 0;
 }
 
 
@@ -52,53 +65,54 @@ void int0() interrupt 0
 //start , stop
 void int1() interrupt 2
 {
-	start_stop_flag = ~start_stop_flag;
-	TR0 = start_stop_flag;
+
+	TR0 = ~TR0;
+	TR1 = ~TR1;
 }
 
+
 //moto turn func
-*/
 void timer0() interrupt 1
 {
 	
-	TH0 = 0xfc;
+	TH0 = 0xfc;  //1ms
 	TL0 = 0x18;
 
 	
 	
 	P2 = turn[i];
 	
-	if(turn_direction_flag == 0){
-		i++;  			//forward
+	if(turn_direction_flag == 1){
+		i--;  			//forward
+		if(i == -1){
+			i = 7;
+		}
 	}
 	
-	else if(turn_direction_flag == 1){
-		i--;				// backward
+	else{
+		i++;				// backward
+		if(i == 8){
+			i = 0;
+		}
 	}
 	
 	
-	if(i == 8){
-		i = 0;
-	}
+
 	
-	if(i == -1){
-		i = 7;
-	}
+
 	
 }
-
-
 void timer1() interrupt 3
 {
-	TH1 = 0x3c;
-	TL1 = 0xb0;
+	TH1 = 0xfc;
+	TL1 = 0x18;
 	
-	speed--;
-	if(speed == 63536){
-		TR1 = 0;
-		ET1 = 0;
+	if(turn_direction_flag == 1){
+		length++;
 	}
-
+	else{
+		length--;
+	}
 }
 
 
@@ -119,4 +133,5 @@ void delay_nms(unsigned short int t)
 		delay1ms();
 	}
 }
+
 */
